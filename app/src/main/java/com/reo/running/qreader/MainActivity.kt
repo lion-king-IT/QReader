5eccc5e1b30aa54e9ef0bcd21c3a78ae8d55558b
github lion-king-IT <reo.runningg@gmail.com>
2020-12-05 19:51:45 +0900

    QRコード読み取って、無言でURL飛ばす実装までできました。

--- app/src/main/java/com/reo/running/qreader/MainActivity.kt
+++ app/src/main/java/com/reo/running/qreader/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.reo.running.qreader
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -15,58 +16,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //認可定数かな
     companion object {
     const val REQUEST_CAMERA_PERMISSION : Int = 1
     }
 
+    //QR読み取り
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
         qrChecker()
         initQRCamera()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode) {
-            REQUEST_CAMERA_PERMISSION -> {initQRCamera()}
-        }
-    }
-
-    @SuppressLint("WringConstant")
-    private fun initQRCamera() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return
-
-        val isReadPermissionGranted = (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-        val isWritePermissionGranted = (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-        val isCameraPermission = (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-
-        if (isReadPermissionGranted && isWritePermissionGranted && isCameraPermission) {
-            openQRCamera()
-        } else {
-            requestPermissions(arrayOf(android.Manifest.permission.CAMERA,android.Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CAMERA_PERMISSION)
-        }
-    }
-
-    private fun openQRCamera() {
-        qr_view.decodeContinuous(object : BarcodeCallback {
-            override fun barcodeResult(result: BarcodeResult?) {
-                if (result != null) {
-                    onPause()
-                    Log.d("QRCode","$result")
-                }
-            }
-
-            override fun possibleResultPoints(resultPoints: MutableList<ResultPoint>?) { }
-        })
-    }
-
-
-
+    //permissionの確認
     private fun qrChecker() {
         if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             qr_view.resume()
@@ -76,4 +39,46 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),999)
         }
     }
+    
+    //QRカメラ初期化
+    @SuppressLint("WrongConstant")
+    private fun initQRCamera() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return
+
+        val isReadPermissionGranted = (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+        val isWritePermissionGranted = (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+        val isCameraPermissionGranted = (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+
+        if (isReadPermissionGranted && isWritePermissionGranted && isCameraPermissionGranted) {
+            openQRCamera() //カメラ起動！！！
+        } else {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA,android.Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CAMERA_PERMISSION)
+        }
+    }
+
+    //こちらは、ユーザーの選択肢を精査する処理
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            REQUEST_CAMERA_PERMISSION -> { initQRCamera() }
+        }
+    }
+
+    //QRカメラ初期化
+    private fun openQRCamera() {
+        qr_view.decodeContinuous(object : BarcodeCallback {
+            override fun barcodeResult(result: BarcodeResult?) {
+                if (result != null) {
+                    onPause()
+                    val uri = Uri.parse("$result")
+                    val intent = Intent(Intent.ACTION_VIEW,uri)
+                    startActivity(intent)
+                }
+            }
+
+            override fun possibleResultPoints(resultPoints: MutableList<ResultPoint>?) { }
+        })
+    }
+
+
 }
